2f4ca1367db3cb82b9b96deae43a38ed3b60d116
larryw <larrywong9898@gmail.com>
2023-12-23 21:38:45 -0600

    add waterdrop

---
+++ widget-next/app/purecss/waterdrop/page.tsx
@@ -0,0 +1,49 @@
+import styles from "./page.module.css";
+
+const Page = () => {
+  return (
+    <div className={styles["base"]}>
+      <div className={styles["center"]}>
+        <div className={styles["waterdrop"]}>
+          <div
+            className={styles["waterdrop-1"] + " " + styles["waterdrop-base"]}
+          ></div>
+          <div
+            className={styles["waterdrop-2"] + " " + styles["waterdrop-base"]}
+          ></div>
+          <div
+            className={styles["waterdrop-3"] + " " + styles["waterdrop-base"]}
+          ></div>
+          <div
+            className={styles["waterdrop-4"] + " " + styles["waterdrop-base"]}
+          ></div>
+          <div
+            className={styles["waterdrop-5"] + " " + styles["waterdrop-base"]}
+          ></div>
+          <div
+            className={styles["waterdrop-6"] + " " + styles["waterdrop-base"]}
+          ></div>
+          <div
+            className={styles["waterdrop-7"] + " " + styles["waterdrop-base"]}
+          ></div>
+          <div
+            className={styles["waterdrop-8"] + " " + styles["waterdrop-base"]}
+          ></div>
+          <div
+            className={styles["waterdrop-9"] + " " + styles["waterdrop-base"]}
+          ></div>
+          <div
+            className={styles["waterdrop-10"] + " " + styles["waterdrop-base"]}
+          ></div>
+          <div
+            className={styles["waterdrop-11"] + " " + styles["waterdrop-base"]}
+          ></div>
+          <div
+            className={styles["waterdrop-12"] + " " + styles["waterdrop-base"]}
+          ></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Page;
